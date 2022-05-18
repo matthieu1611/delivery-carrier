53491c16c9241722bce3f98df3843061f451bfa5
Florian da Costa <florian.dacosta@akretion.com>
2022-05-18 12:57:41 +0200

    [FIX] GLS Manage size contrains on street, name and company

--- delivery_roulier_gls_fr/models/picking.py
+++ delivery_roulier_gls_fr/models/picking.py
@@ -1,10 +1,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import logging
-from datetime import date
 
-from odoo import fields, models
-from odoo.exceptions import UserError
+from odoo import models
 
 _logger = logging.getLogger(__name__)
 
@@ -21,23 +19,33 @@ class StockPicking(models.Model):
     def _gls_fr_rest_get_to_address(self, package=None):
         address = self._roulier_get_to_address(package=package)
         (
-            address["street"],
+            address["street1"],
             address["street2"],
             address["street3"],
         ) = self.partner_id._get_split_address(3, 35)
+        # manage min/max size for company and name (2-35)
+        keys = ["name", "company"]
+        for key in keys:
+            size = len(address.get(key, ""))
+            if size and size < 2:
+                address[key] = ""
+            if size and size > 35:
+                address[key] = address[key][0:35]
         return address
 
     def _gls_fr_rest_get_service(self, account, package=None):
         result = self._roulier_get_service(account, package=package)
-        result.update({
-            # should probably renamed contactId in roulier
-            "agencyId": account.gls_fr_rest_contact_id,
-            "customerId": account.gls_fr_rest_customer_id,
-            "intructions": self.note or "",
-            "consignee_ref": self.name[:20],
-            "reference_1": self.origin[:20],
-            "reference_2": self.name[:20],
-        })
+        result.update(
+            {
+                # should probably renamed contactId in roulier
+                "agencyId": account.gls_fr_rest_contact_id,
+                "customerId": account.gls_fr_rest_customer_id,
+                "intructions": self.note or "",
+                "consignee_ref": self.name[:20],
+                "reference_1": self.origin[:20],
+                "reference_2": self.name[:20],
+            }
+        )
         incoterm_code = self._get_gls_incoterm()
         if incoterm_code:
             result["incoterm"] = incoterm_code
@@ -50,4 +58,3 @@ class StockPicking(models.Model):
         if incoterm:
             gls_code = INCOTERM_MAPPING.get(incoterm.code, "")
         return gls_code
-
